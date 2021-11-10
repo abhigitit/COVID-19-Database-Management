468ee0fdae03c9983ec7bd6774a51df80b4b6f78
Santhosh <santhoshsai3@gmail.com>
2021-11-10 04:42:52 -0800

    added login in backend

--- backend/Routes/login.js
+++ backend/Routes/login.js
@@ -1,61 +1,27 @@
 const express = require("express");
-
 const router = express.Router();
-
 const bcrypt = require("bcrypt");
-
 const pool = require("../pool.js");
 
-const bodyParser = require("body-parser");
-
-router.post("/login", (req, res) => {
-  console.log("from frontend" + req.body);
-  pool.query(
-    "SELECT * from person where p_id = ?",
-    [req.body.email_id],
-    (err, result) => {
-      if (err) {
-        res.send("Error");
-      }
-      console.log("result" + result.length);
-
-      if (result && result.length > 0) {
-        bcrypt.compare(
-          req.body.password,
-          result[0].password,
-          (err, response) => {
-            console.log("response" + response);
-            if (response) {
-              console.log("Got response");
-
-              let userObject = { email_id: result[0].p_id };
-
-              res.writeHead(200, {
-                "Content-Type": "text/plain",
-              });
-
-              res.end(JSON.stringify(userObject));
-            } else {
-              res.writeHead(401, {
-                "Content-Type": "text/plain",
-              });
-
-              res.end("INCORRECT_PASSWORD");
-              console.log("incorrect password");
-            }
-          }
-        );
-      } else if (result.length == 0) {
-        res.send("email not found"); //this is what you are missing
-      } else {
-        res.writeHead(401, {
-          "Content-Type": "text/plain",
-        });
-
-        res.end("NO_USER");
-      }
+router.post("/", (req, res) => {
+  const enteredPassword = req.body.Password;
+  pool.query("SELECT * from person where p_id = ?",[req.body.Email],(err,result)=>{
+    if (err || Object.keys(result).length === 0) {
+      res.status(401).send("authentication failed");
     }
-  );
+    else{
+    const hashedPassword = result[0].password;
+    bcrypt.compare(enteredPassword, hashedPassword, function(err, isMatch) {
+      if (err ||!isMatch) {
+        res.status(401).send('authentication failed');
+      } 
+      else {
+        res.status(200).send('Login Successful');
+      }
+    })}
+  });
+
+  
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ const request = require("request");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const register = require('./Routes/register');
+const login = require('./Routes/login');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/register',register);
+app.use('/login',login);
 
 
 const PORT = process.env.PORT || 5000;
